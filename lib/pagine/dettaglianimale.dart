9f636e4417f9a61e1418bab0eac08bdad4df3ff7
mattysaints <tbarbera98@gmail.com>
2022-03-27 01:23:46 +0100

    Implementato inserimento patologie tramite dropdown textfield, da modificare qualcosa di grafica ma per il resto tutto funzionante

--- lib/pagine/dettaglianimale.dart
+++ lib/pagine/dettaglianimale.dart
@@ -27,6 +27,8 @@ class _DettagliAnimaleState extends State<DettagliAnimale> {
   final TextEditingController peso = TextEditingController();
   final TextEditingController razza = TextEditingController();
   late bool peloPungo = this.widget.animale.peloLungo;
+  late List<TextEditingController> _patologie = [TextEditingController()];
+  late List<String> patologia = [];
   HttpService httpService = HttpService();
 
 
@@ -36,7 +38,14 @@ class _DettagliAnimaleState extends State<DettagliAnimale> {
     if(!widget.nuovo){
       nome.text = widget.animale.nome ?? '';
       data.text = widget.animale.dataDiNascita.toString().substring(0,10) ?? '';
-      patologie.text = widget.animale.patologie.join(',').toString() ?? '';
+      if(widget.animale.patologie.isNotEmpty) {
+        _patologie.clear();
+        for (var patologia in widget.animale.patologie){
+          final controller = TextEditingController();
+          controller.text = patologia;
+          _patologie.add(controller);
+        }
+      }
       razza.text = widget.animale.razza ?? '';
       peso.text = widget.animale.peso.toString() ?? '';
     }
@@ -54,12 +63,19 @@ class _DettagliAnimaleState extends State<DettagliAnimale> {
         onPressed: () async {
           late Animale _animale;
           if(widget.nuovo){
-            _animale = Animale(0, nome.text, DateTime.parse(data.text), patologie.text.split(","), razza.text, num.parse(peso.text), peloPungo);
+            for(var pat in _patologie){
+              patologia.add(pat.text);
+            }
+            _animale = Animale(0, nome.text, DateTime.parse(data.text), patologia, razza.text, num.parse(peso.text), peloPungo);
             widget.utente = await httpService.addAnimal(widget.utente, _animale);
             Navigator.push(context, new MaterialPageRoute(builder: (__) => new Dashboard(widget.utente)));
           } else {
-            //httpService.updateAnimal(widget.animale,_animale);
-            _animale = Animale(widget.animale.id, nome.text, DateTime.parse(data.text), patologie.text.split(","), razza.text, num.parse(peso.text), peloPungo);
+            for(var pat in _patologie){
+              if(pat.text != ""){
+                patologia.add(pat.text);
+              }
+            }
+            _animale = Animale(widget.animale.id, nome.text, DateTime.parse(data.text), patologia, razza.text, num.parse(peso.text), peloPungo);
             _animale = await httpService.updateAnimal(widget.utente,widget.animale,_animale);
             widget.utente.animali.remove(widget.animale);
             widget.utente.animali.add(_animale);
@@ -67,45 +83,101 @@ class _DettagliAnimaleState extends State<DettagliAnimale> {
           }
         },
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            CasellaTesto(nome, "Nome"),
-            Padding(
-              padding: EdgeInsets.all(16),
-              child: TextField(
-                controller: data,
-                decoration: InputDecoration(
-                    icon: Icon(Icons.calendar_today),
-                  labelText: "Data"
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: SingleChildScrollView(
+          child: Card(
+            child: Column(
+              children: <Widget>[
+                CasellaTesto(nome, "Nome"),
+                Padding(
+                  padding: EdgeInsets.all(16),
+                  child: TextField(
+                    controller: data,
+                    decoration: InputDecoration(
+                        icon: Icon(Icons.calendar_today),
+                      labelText: "Data"
+                    ),
+                    readOnly: false,
+                    onTap: () async {
+                      DateTime? date = await showDatePicker(context: context, initialDate: DateTime.now(), firstDate: DateTime(2000), lastDate: DateTime(2101));
+                      if(date!=null){
+                        String formattedDate = DateFormat('yyyy-MM-dd').format(date);
+                        setState(() {
+                          data.text = formattedDate;
+                        });
+                      }
+                    },
+                  ),
                 ),
-                readOnly: false,
-                onTap: () async {
-                  DateTime? date = await showDatePicker(context: context, initialDate: DateTime.now(), firstDate: DateTime(2000), lastDate: DateTime(2101));
-                  if(date!=null){
-                    String formattedDate = DateFormat('yyyy-MM-dd').format(date);
+                ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: _patologie.length,
+                  itemBuilder: (context,index){
+                    return addPatologia(_patologie[index], "Patologia",index+1);
+                  },
+                ),
+                CasellaTesto(razza, "Razza"),
+                CasellaTesto(peso, "Peso"),
+                CheckboxListTile(
+                  title: Text("Pelo lungo"),
+                  checkColor: Colors.greenAccent,
+                  value: this.peloPungo,
+                  onChanged: (bool? value){
                     setState(() {
-                      data.text = formattedDate;
+                      this.peloPungo = value!;
                     });
-                  }
-                },
-              ),
+                  },
+                )
+              ],
             ),
-            CasellaTesto(patologie, "Patologie"),
-            CasellaTesto(razza, "Razza"),
-            CasellaTesto(peso, "Peso"),
-            CheckboxListTile(
-              title: Text("Pelo lungo"),
-              checkColor: Colors.greenAccent,
-              value: this.peloPungo,
-              onChanged: (bool? value){
-                setState(() {
-                  this.peloPungo = value!;
-                });
-              },
-            )
-          ],
+          ),
         ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+
+  addPatologia(TextEditingController patologie, String s, int index) {
+    return Padding(
+      padding: EdgeInsets.all(16),
+      child: Row(
+          children: [
+            Text('Patologia: $index'),
+            SizedBox(width: 10),
+            Expanded(
+              child: TextField(
+                autofocus: true,
+                textAlign: TextAlign.center,
+                controller: patologie,
+                decoration: InputDecoration(
+                  suffixIcon: Container(
+                    child: IntrinsicHeight(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          IconButton(onPressed: (){
+                            if(_patologie.length>1){
+                              setState(() {
+                                  _patologie.removeAt(index-1);
+                              });
+                            }
+                          }, icon: Icon(Icons.clear)),
+                          IconButton(onPressed: (){
+                            setState(() {
+                              final controller = TextEditingController();
+                              _patologie.add(controller);
+                            });
+                          }, icon: Icon(Icons.add)),
+                        ],
+                      ),
+                    ),
+                  )
+                ),
+              ),
+            )
+          ]
       ),
     );
   }
@@ -130,4 +202,5 @@ class CasellaTesto extends StatelessWidget {
     );
   }
 }
+
 
